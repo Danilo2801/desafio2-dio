c52463fccb56e0f9379a82539bcba2312beec270
Danilo2801 <danilo.gomes.santana94@gmail.com>
2024-02-15 21:20:45 -0300

    Finalizado com algumas alterações

--- desafio2.js
+++ desafio2.js
@@ -1,8 +1,27 @@
-let pontos = calcular (150, 55)
-let rank
 
-function calcular(vitorias, derrotas) {
-    let sub = vitorias - derrotas
-    let rank
+function calcularNivel(vitorias, derrotas) {
 
+    let saldoVitorias = vitorias - derrotas;
+    let nivel; 
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return { saldoVitorias, nivel };
 }
+
+let resultado = calcularNivel(150, 5);
+console.log(`O Herói tem um saldo de ${resultado.saldoVitorias} e está no nível de ${resultado.nivel}`);
